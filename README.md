2c0b04c4dc43cf08dfb16d1bccecd343c7f95b17
foster999 <foster.dev999@gmail.com>
2021-01-31 15:04:07 +0000

    Add sh script outline

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # git-assumptions
 Git extension for generating an assumptions log from inline comments.
+
+Assumptions should be written inline in code using the format:
+
+```
+# Assumption: Title of assumption
+# Detailed description
+# on next line or many
+
+# Assumption: Another assumption
+# Leaving an empty newline after
+# the previous one
+print("But code doesn't require a newline")
+```
---
+++ bin/git-assumptions.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+
+set -e
+
+usage() {
+    echo ""
+    echo "Syntax: git assumptions [-h|f|t] "
+    echo "Options:"
+    echo "h     show this help."
+    echo "f     specify the filename of the output Markdown file"
+    echo "      to write the assumptions log to. Defaults to "
+    echo "      'assumptions_log.md'."
+    echo "t     target is the directory to search for assumptions."
+    echo "      Defaults to current dir."
+    # echo "  See 'man git-assumptions' for further information"
+}
+
+
+write_md_page()
+{
+    echo "# Assumptions and Caveats Log
+
+## Assumptions
+
+## Caveats
+"
+}
+
+# Parse options
+while [ "$1" != "" ]; do
+    case $1 in
+        -f | --file )           shift
+                                filename=$1
+                                ;;
+        -t | --target )         shift
+                                target=$1
+                                ;;
+        -h | --help )           usage
+                                exit
+                                ;;
+        * )                     usage
+                                exit 1
+    esac
+    shift
+done
+
+# Set defaults
+filename=${filename:-"asumptions_log.md"}
+target=${target:-"./"}
+
+echo "Searching for assumptions under: $target"
+
+echo "Writing assumptions log to: $filename"
+
+write_md_page > $filename
