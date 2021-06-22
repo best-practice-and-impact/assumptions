3b96c06332f1537465f652b7701434b6c3a5c00c
foster999 <foster.dev999@gmail.com>
2021-06-22 20:36:52 +0100

    Change search pattern to allow multiple patterns

--- tests/test_log_items.py
+++ tests/test_log_items.py
@@ -4,11 +4,13 @@ from assumptions.log_items import LogItem
 
 
 def test_incomplete_subclass():
+    """Test that subclass fails if abstract properties not implemented."""
+
     class LazyLogItem(LogItem):
         pass
 
     with pytest.raises(
         TypeError,
-        match="empty_message, parse, search_pattern, template_marker",
+        match="empty_message, parse, search_patterns, template_marker",
     ):
         instance = LazyLogItem()  # noqa: F841
