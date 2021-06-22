d5c1c615e04e65ecc6d2213eb4efad3f0b58ef74
foster999 <foster.dev999@gmail.com>
2021-06-22 21:06:08 +0100

    Update docs with R roxygen examples

---
+++ docs/source/example_assumptions.py
@@ -0,0 +1,20 @@
+# Assumption: Title of assumption
+# Quality: RED
+# Impact: AMBER
+# Detailed description
+# on next line or many.
+# Assumption: Another assumption
+# Q: GREEN
+# I: RED
+# Leaving an empty newline after
+# the previous one.
+#
+# Q and I can be used for shorthand RAG ratings.
+print("Code doesn't require a newline")
+
+# Assumption: Yet another assumption
+# Q: RED
+# I: GREEN
+# Indented? No problem.
+
+# But non-assumption comments do require an empty newline.
